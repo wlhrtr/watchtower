1721371db93863d07b118becb81f3ce80e7f2d4b
Peter Weilharter <pw@webundsoehne.com>
2022-09-14 14:45:43 +0200

    adds support for older versions

--- config/watchtower.php
+++ config/watchtower.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'url' => env('WATCHTOWER_URL'),
-    'client_id' => env('WATCHTOWER_CLIENT_ID'),
-    'client_secret' => env('WATCHTOWER_CLIENT_SECRET'),
+    'url' => env('WATCHTOWER_URL', null),
+    'client_id' => env('WATCHTOWER_CLIENT_ID', null),
+    'client_secret' => env('WATCHTOWER_CLIENT_SECRET', null),
     'environment' => env('WATCHTOWER_ENVIRONMENT', config('app.env')),
 ];
--- src/Services/WatchtowerService.php
+++ src/Services/WatchtowerService.php
@@ -4,6 +4,7 @@ namespace Wlhrtr\Watchtower\Services;
 
 use Exception;
 use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class WatchtowerService
 {
@@ -13,38 +14,54 @@ class WatchtowerService
     private string $clientId;
     private string $clientSecret;
 
+    private $client;
 
     public function __construct(string $url, string $clientId, string $clientSecret)
     {
         $this->url = $url;
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
+
+        $this->client = new Client(['base_uri' => $url]);
+
         $this->auth();
     }
 
     public function send(array $data): bool
     {
-        $response = Http::acceptJson()
-            ->withToken($this->token)
-            ->post($this->url . '/api/notify', $data);
+        $response = $this->client->post($this->url . '/api/notify', [
+            'json' => $data,
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => "Bearer {$this->token}",
+            ]
+        ]);
 
-        if ($response->status() === 401) {
+        if ($response->getStatusCode() === 401) {
             $this->auth();
             $this->send($data);
         }
 
-        return $response->status() === 200 ? true : false;
+        return $response->getStatusCode() === 200 ? true : false;
     }
 
     private function auth(): void
     {
-        $response = Http::acceptJson()->post($this->url . '/oauth/token', [
-            'grant_type' => 'client_credentials',
-            'client_id' => $this->clientId,
-            'client_secret' => $this->clientSecret
+        $response = $this->client->post($this->url . '/oauth/token', [
+            'json' => [
+                'grant_type' => 'client_credentials',
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret
+            ]
         ]);
 
-        $data = json_decode((string)$response);
+        match ($response->getStatusCode()) {
+            404 => throw new Exception('Auth URL not found. Check ENV vars.'),
+            401 => throw new Exception('Auth failed. Please check your credentials.'),
+            default => null
+        };
+
+        $data = json_decode((string)$response->getBody());
 
         $this->token = $data->access_token;
     }
